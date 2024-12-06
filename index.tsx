f4b8fb88ff3a6cd12f81977f43721b505b20acab
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-05 20:45:42 -0500

    create plugin

--- index.tsx
+++ index.tsx
@@ -1,12 +1,28 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
+import { Icons, Menu } from "@webpack/common";
 
 export default definePlugin({
     name: "CopyFolderId",
-    description: "",
-    authors: [
-        {
-            name: "sadan",
-            id: 521819891141967883n
+    description: "Adds an option to copy folder ids. This isnt used that much, but when you need it its really annoying to get.",
+    authors: [Devs.sadan],
+
+    contextMenus: {
+        "guild-context"(arr, { folderId }: { folderId?: number }) {
+            if(!folderId) return;
+
+            arr.push((<Menu.MenuItem
+                id="vc-copyFolderId"
+                label="Copy Folder ID"
+                icon={Icons.IdIcon}
+            />));
         }
-    ]
-})
+    }
+});
+
